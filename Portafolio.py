0a9f9a5775d0517347c8421acdf6297dd3ccbd29
LaraBerenice <berenice.ledesma12345@gmail.com>
2025-04-22 19:51:39 -0300

    subida

--- Portafolio.py
+++ Portafolio.py
@@ -248,8 +248,8 @@ elif st.session_state.seccion == "Proyectos":
         <div style='background-color: rgba(255, 255, 255, 0.4); padding: 5px; border-radius: 10px;'>
             <h4>Negocio de Comida Rápida</h4>
             <p>El negocio buscaba organizar sus registros en una base de datos,
-            así que creé la base, la modelé de manera óptima para facilitar el análisis
-            de la información e inserté los datos.</p>
+            las soluciones fueron creación la base de datos, la modelado de manera
+            el análisis de la de la información y inserción de los datos.</p>
             <a href="https://github.com/LaraBerenice/Descubriendo_la_BD_Fast_Food" style="color: #003366; font-weight: bold;">
             Ver más ... </a>
 
