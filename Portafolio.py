2a2e537125e9b754ab0854da77c000455f808db7
LaraBerenice <berenice.ledesma12345@gmail.com>
2025-05-14 21:04:20 -0300

    mas

--- Portafolio.py
+++ Portafolio.py
@@ -7,7 +7,7 @@ import requests
 from urllib.parse import quote
 
 # Configuración de la página
-st.set_page_config(page_title="Análisis de Datos, Agronegocios y Gestión Ambiental", layout="centered")
+st.set_page_config(page_title="Análisis de Datos, Agronegocios y Gestión Ambiental", layout="wide")
 
 def imagen_base64(ruta):
     with open(ruta, "rb") as img_file:
